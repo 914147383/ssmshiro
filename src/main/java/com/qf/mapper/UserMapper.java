fa0a7cb17983a902976b762e75ed420d6d0e89a1
914147383 <914147383@qq.com>
2019-11-11 21:40:47 +0800

    service

---
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.qf.mapper;
+
+import com.qf.pojo.User;
+import org.apache.shiro.SecurityUtils;
+
+import java.util.List;
+
+public interface UserMapper {
+    public String getPassword(String username);
+
+    public String getRoleName(String username);
+
+    public User getUserById(int uid);
+
+    public List<User> getUserList();
+
+    public List<User> getUserListByRoleName(String rolename);
+
+    public int deleteUser(int uid);
+
+    public int updatePassword(User user);
+
+    public int addUser(User user);
+
+}
